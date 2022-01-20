89444487ecf18ac3fa5ff1a471c4c1211c98a6ae
Marcel Arns <marcel.arns@moneymeets.com>
2022-01-20 14:03:49 +0100

    feat(action): rebuild to action use new Beanstalk platform with Amazon Linux 2

--- action_test.py
+++ action_test.py
@@ -1,113 +1,119 @@
-import hashlib
+from dataclasses import replace
+from datetime import datetime, timedelta
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Optional, Sequence
 from unittest import TestCase
 from unittest.mock import patch
 
 import action
-from action import ContainerConfig, ContainerMount, create_beanstalk_application_version, prepare_dockerrun_file
+
+MOCK_CONFIG = action.Config(
+    application_name="demo-app",
+    description="Test deploy",
+    docker_compose_path=Path("docker/docker-compose.yml"),
+    environment_name="demo-environment",
+    platform_hooks_path=Path("platform-hooks"),
+    region="eu-central-1",
+    version_label="abc123456",
+)
+MOCK_APPLICATION = action.BeanstalkApplication(MOCK_CONFIG.application_name)
+MOCK_ENVIRONMENT = action.BeanstalkEnvironment(MOCK_APPLICATION, MOCK_CONFIG.environment_name)
+MOCK_APPLICATION_VERSION = action.ApplicationVersion(MOCK_APPLICATION, "version-0", "PROCESSED")
+MOCK_TIME = datetime.utcnow()
 
 
-class MountsTest(TestCase):
-    HOST_VOLUME_PATH = "/var/my-sample-application-test/data"
-    HOST_VOLUME_NAME = hashlib.sha1(HOST_VOLUME_PATH.encode()).hexdigest()
-    APP_NAME = "sample-application"
-    IMAGE_NAME = "YourAwsAccountId.dkr.ecr.YourAwsRegion.amazonaws.com/your-ecr-repository"
-
-    def _create_container_config(self, container_path: str):
-        return ContainerConfig(
-            dockerfile="docker/Dockerfile",
-            name=self.APP_NAME,
-            image_base_name=self.IMAGE_NAME,
-            ports=(),
-            mounts=(
-                ContainerMount(
-                    host_path=self.HOST_VOLUME_PATH,
-                    container_path=container_path,
-                    read_only=False,
+class TestApplicationVersion(TestCase):
+    def _get_or_create_application_version(self, polling_results: Sequence[Optional[action.ApplicationVersion]]):
+        with NamedTemporaryFile() as docker_compose_file, patch.object(action, "boto3"), patch.object(
+            action.ApplicationVersion,
+            "get",
+            side_effect=polling_results,
+        ) as mock_get_application_version:
+            result = action.get_or_create_beanstalk_application_version(
+                MOCK_APPLICATION,
+                replace(
+                    MOCK_CONFIG,
+                    docker_compose_path=Path(docker_compose_file.name),
                 ),
+                polling_interval=timedelta(0),
+                polling_max_steps=len(polling_results) - 1,
+            )
+            self.assertEqual(mock_get_application_version.call_count, len(polling_results))
+            return result
+
+    def test_version_exists(self):
+        with self.assertLogs() as captured:
+            result = self._get_or_create_application_version((MOCK_APPLICATION_VERSION,))
+            self.assertEqual(result, MOCK_APPLICATION_VERSION)
+            self.assertEqual(
+                captured.records[0].getMessage(),
+                f"Application version {MOCK_CONFIG.version_label} already exist",
+            )
+
+    def test_create_new_version(self):
+        result = self._get_or_create_application_version(
+            (
+                None,
+                None,
+                replace(MOCK_APPLICATION_VERSION, status="PROCESSING"),
+                MOCK_APPLICATION_VERSION,
             ),
         )
+        self.assertEqual(result, MOCK_APPLICATION_VERSION)
 
-    def _create_dockerrun_config(self, app_version: str, *mounts: str):
-        return {
-            "AWSEBDockerrunVersion": 2,
-            "volumes": [
-                {
-                    "name": self.HOST_VOLUME_NAME,
-                    "host": {
-                        "sourcePath": self.HOST_VOLUME_PATH,
-                    },
-                },
-            ],
-            "containerDefinitions": [
-                {
-                    "name": self.APP_NAME,
-                    "image": f"{self.IMAGE_NAME}:{app_version}",
-                    "environment": [],
-                    "essential": True,
-                    "links": [],
-                    "memoryReservation": 1024,
-                    "portMappings": [],
-                    "mountPoints": [
-                        {
-                            "sourceVolume": self.HOST_VOLUME_NAME,
-                            "containerPath": mount,
-                            "readOnly": False,
-                        },
-                    ],
-                }
-                for mount in mounts
-            ],
-        }
-
-    def _test_single_host_path(self, app_version: str, *container_mounts: str):
-        containers = tuple(self._create_container_config(mount) for mount in container_mounts)
-
-        self.assertDictEqual(
-            prepare_dockerrun_file(containers, app_version),
-            self._create_dockerrun_config(app_version, *container_mounts),
-        )
-
-    def test_single_mount_conversion(self):
-        self._test_single_host_path("app-version", "/opt/data")
-
-    def test_host_path_reuse(self):
-        self._test_single_host_path("other-app-version", "/opt/data", "/opt/other-data")
-
-
-@patch.object(action, "boto3")
-class ApplicationVersionTest(TestCase):
-    @patch.object(
-        action,
-        "get_application_versions",
-        side_effect=[
-            [],
-            [{"Status": "PROCESSING"}],
-            [{"Status": "PROCESSED"}],
-        ],
-    )
-    def test_successful_response(self, mock_get_application_versions, *_):
-        create_beanstalk_application_version(
-            app="APP",
-            version="abc12345",
-            description="",
-            bucket="bucket",
-            archive="deploy-abc12345.zip",
-        )
-        self.assertEqual(mock_get_application_versions.call_count, 3)
-
-    @patch.object(
-        action,
-        "get_application_versions",
-        side_effect=[[{"Status": "PROCESSING"}] for i in range(4)],
-    )
-    def test_timeout_error(self, *_):
+    def test_timeout_error(self):
         with self.assertRaises(TimeoutError):
-            create_beanstalk_application_version(
-                app="APP",
-                version="abc12345",
-                description="",
-                bucket="bucket",
-                archive="deploy-abc12345.zip",
-                max_retries=3,
-                wait_time=0,
+            self._get_or_create_application_version((None,) * 5)
+
+
+class TestDeployment(TestCase):
+    def _deploy(self, get_health_return_values: Sequence[dict], is_active_in_environment: bool):
+        with patch.object(
+            action.BeanstalkEnvironment,
+            "get_health",
+            side_effect=get_health_return_values,
+        ) as mock_get_health, patch.object(
+            action.BeanstalkEnvironment,
+            "get_events",
+            return_value=(),
+        ) as mock_get_events, patch.object(
+            action.ApplicationVersion,
+            "is_active_in_environment",
+            return_value=is_active_in_environment,
+        ) as mock_is_active_in_environment, patch.object(
+            action,
+            "boto3",
+        ):
+            iterations = len(get_health_return_values)
+
+            MOCK_APPLICATION_VERSION.deploy_to_environment(
+                MOCK_ENVIRONMENT,
+                polling_interval=timedelta(0),
+                polling_max_steps=iterations,
             )
+
+            self.assertEqual(mock_get_health.call_count, iterations)
+            self.assertEqual(mock_get_events.call_count, iterations)
+            mock_is_active_in_environment.assert_called_once()
+
+    def test_successful_deployment(self):
+        self._deploy(
+            (
+                {"Status": "InProgress", "Color": "Grey"},
+                {"Status": "Ready", "Color": "Green"},
+            ),
+            True,
+        )
+
+    def test_environment_timeout_error(self, *_):
+        with self.assertRaises(TimeoutError):
+            self._deploy(({"Status": "InProgress", "Color": "Grey"},) * 5, True)
+
+    def test_health_failed(self):
+        with self.assertRaises(RuntimeError):
+            self._deploy(({"Status": "Ready", "Color": "Red"},), True)
+
+    def test_version_not_active(self):
+        with self.assertRaises(RuntimeError):
+            self._deploy(({"Status": "Ready", "Color": "Green"},), False)
